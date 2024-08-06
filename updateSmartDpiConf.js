fdd3fc669bc738aded482c26dacd5e672be79eeb
lielbe <lielbe@checkpoint.com>
2024-08-06 15:53:48 +0300

    remove start from list

--- updateSmartDpiConf.js
+++ updateSmartDpiConf.js
@@ -471,7 +471,7 @@ function createTimeLine(){
         var item = items.get(properties.item);
         var details = `
             <p><strong>Disabled Protections</strong></p>
-            <ul>${item.info.map(info => `<li>${info}</li>`).join('')}</ul>
+            <ul class="items-list">${item.info.map(info => `<li>${info}</li>`).join('')}</ul>
         `;
         document.getElementById('item-details').innerHTML = details;
         document.getElementById('item-modal').style.display = 'block';
